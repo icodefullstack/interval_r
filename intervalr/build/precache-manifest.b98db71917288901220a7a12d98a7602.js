8d00bcb7b83c9a7f970bf9871ddd4289e852d5c7
ion comanici <ioncomanici@gmail.com>
2018-11-06 22:25:53 +0000

    set up server

---
+++ intervalr/build/precache-manifest.b98db71917288901220a7a12d98a7602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9232f84ec90b64b7f98e",
+    "url": "/static/js/main.9232f84e.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "9232f84ec90b64b7f98e",
+    "url": "/static/css/main.2deec184.chunk.css"
+  },
+  {
+    "revision": "416da10cdfa16774566d574ffb9b8864",
+    "url": "/index.html"
+  }
+];
