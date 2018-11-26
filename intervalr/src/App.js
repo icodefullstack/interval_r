b76ad1f70547e7df0d60f2ee9828f568c4122d76
ion comanici <ioncomanici@gmail.com>
2018-11-26 22:37:03 +0000

    pushing fromteclast

--- intervalr/src/App.js
+++ intervalr/src/App.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
-import logo from './assets/graphics/images/logo.svg';
 import './App.css';
-import Drawer from './drawer/Drawer.js';
 import Selector from './drawer/Selector.js';
 
 class App extends Component {
   render() {
     return(
       <div id="App">
-        {/*
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Welcome to IntervalR</h1>
-          </header>
-        */}
-
+<button onClick={()=>{alert('test')}}>test</button>
         <div id="chest">
         <Selector></Selector>
-  {/*
-        <Drawer title="Backgrounds"></Drawer>
-        <Drawer title="Notifications"></Drawer>
-        <Drawer title="Numbers"></Drawer>
-        <Drawer title="Commands"></Drawer>
-  //*/}
-
         </div>
-        <div id="composition">
-          <div id="background_s"></div>
-          <div id="track">
-          <div id="tack_start"></div>
-          <div id="track_body"></div>
-          <div id="track_end"></div>
-          </div>
-        </div>
+        
       </div>
     )
   }
